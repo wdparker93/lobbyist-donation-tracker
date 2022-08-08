86530f349280334b3357ac5493f0649c5d862431
wdparker <wdparker@ncsu.edu>
2022-08-08 17:47:13 -0500

    Initial project creation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>A Picture Lived Here</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
