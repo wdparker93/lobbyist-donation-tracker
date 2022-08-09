9fbdccce4bfddb9e3c278760e6f2cbd045127010
wdparker <wdparker@ncsu.edu>
2022-08-08 23:12:55 -0500

    Styled page and added selection windows

--- src/App.js
+++ src/App.js
@@ -3,17 +3,104 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>A Picture Lived Here</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header" id="main-header">
+        <div id="main-headers">
+          <h1 id="main-title">How Did They Vote?</h1>
+          <h1 id="data-source-title">Where Does This Information Come From?</h1>
+        </div>
+        <div id="secondary-headers">
+          <h2 id="secondary-title">
+            See how your elected officials are voting - fast and easy.
+          </h2>
+          <h2 id="data-source-explanation">
+            The voting data presented by this page are publicly available and
+            can be found at{" "}
+            <a id="congress-gov-link" href="https://www.congress.gov/">
+              congress.gov
+            </a>
+            .
+          </h2>
+        </div>
+      </div>
+      <div id="divider-wrapper">
+        <div id="header-selection-dividers">
+          <hr id="header-selection-divider-1" align="left"></hr>
+          <hr id="header-selection-divider-2" align="right"></hr>
+        </div>
+      </div>
+      <br></br>
+      <div id="selection-criteria-wrapper">
+        <div class="selector-wrapper" id="state-selector-wrapper">
+          <p id="state-selector-label">Select State</p>
+          <select id="state-selector">
+            <option value="--">--</option>
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+          </select>
+        </div>
+        <div class="selector-wrapper" id="issue-selector-wrapper">
+          <p id="issue-selector-label">Select Issue or Enter Custom</p>
+          <input type="text" list="issue-list" />
+          <datalist id="issue-list">
+            <option>Abortion</option>
+            <option>Gun Control</option>
+            <option>Climate Change</option>
+            <option>War in Ukraine</option>
+            <option>January 6th, 2021</option>
+            <option>Education</option>
+            <option>LGBTQ Rights</option>
+            <option>Immigration</option>
+          </datalist>
+        </div>
+      </div>
     </div>
   );
 }
